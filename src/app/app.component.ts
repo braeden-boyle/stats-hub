e318c88999706d8104ec97e32ed323d6e10b0d17
braeden-boyle <2braedenboyle@gmail.com>
2024-09-21 13:31:19 -0400

    Added Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: 'app.component.css'
 })
